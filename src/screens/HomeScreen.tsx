969b2d7ea69b0af3086201d1fbb53bdf5c928407
Ata Güler <seninmailadres ataguler0@gmail.com>
2025-06-11 20:17:51 +0300

    v6: anasayfa logo eklendi

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/AppNavigator';
 
@@ -8,6 +8,11 @@ type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 export default function HomeScreen({ navigation }: Props) {
   return (
     <View style={styles.container}>
+      <Image
+        source={require('../assets/logo2.jpg')}
+        style={styles.logo}
+      />
+
       <Text style={styles.title}>
         Intelligent Dermatologic{'\n'}Diseases Diagnosis App
       </Text>
@@ -33,6 +38,7 @@ export default function HomeScreen({ navigation }: Props) {
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 },
+  logo: { width: 150, height: 150, marginBottom: 20, resizeMode: 'contain' },
   title: { fontSize: 20, fontWeight: 'bold', marginBottom: 10, textAlign: 'center' },
   info: { fontSize: 14, color: '#555', textAlign: 'center', marginBottom: 10 },
   disease: { fontSize: 14, color: '#222', marginTop: 2 }
